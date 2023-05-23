b5b9de9359ff4c280f6a78997b7b4fb18fb25061
marcos andre <marcusandrealves212@gmail.com>
2023-05-23 11:24:22 -0300

    delete main

--- main.js
+++
@@ -1,8 +0,0 @@
-const btnMobile = window.document.getElementsByClassName('btn-mobile')[0];
-    function toggleMenu(){
-    const lista = document.getElementById('nav');
-    lista.classList.toggle('active');
-}
-
-
-btnMobile.addEventListener('click', toggleMenu)
